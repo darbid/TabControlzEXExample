394d3396dfa1027c0aa4909899a2e5e165767e56
David Bowen <maildarbid@icloud.com>
2021-04-06 12:30:08 +0200

    Add project files.

---
+++ BaseViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabControlzEXExample
+{
+    class BaseViewModel : INotifyPropertyChanged
+    {
+        public BrowserTabTypes BrowserTabType { get; set; }
+
+        public enum BrowserTabTypes
+        {
+            MainWPFTabBrowser,
+            OtherBrowser
+        }
+
+        #region "Notify Property Changed"
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion //"Notify Property Changed"
+    }
+}
---
+++ MainWindowViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TabControlzEXExample
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        
+        private ObservableCollection<BaseViewModel> _TabCollection;
+        public ObservableCollection<BaseViewModel> TabCollection
+        {
+            get { return _TabCollection ??= new ObservableCollection<BaseViewModel>(); }
+
+            set
+            {
+                _TabCollection = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private int _BrowserTabControlSelectedIndex = 0;
+        public int BrowserTabControlSelectedIndex
+        {
+            get { return _BrowserTabControlSelectedIndex; }
+            set
+            {
+                _BrowserTabControlSelectedIndex = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            BrowserTabItemViewModel btivm = new();
+            btivm.BrowserTabType = BaseViewModel.BrowserTabTypes.MainWPFTabBrowser;
+            
+            TabCollection.Add(btivm);
+
+            this.BrowserTabControlSelectedIndex = 0;
+
+
+        }
+
+        #region "Notify Property Changed"
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion //"Notify Property Changed"
+    }
+}
